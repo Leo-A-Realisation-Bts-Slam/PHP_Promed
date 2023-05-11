e1e92e4b13f57202841102b7dc604d7e9948e319
Leo-A-Realisation-Bts-Slam <130858701+Leo-A-Realisation-Bts-Slam@users.noreply.github.com>
2023-05-11 19:00:43 +0200

    Delete templates directory

--- templates/articles/accueil.html.php
+++
@@ -1,8 +0,0 @@
-<div class="pictoPraticienPatient d-flex justify-content-center mt-5">
-    <a href=".?controller=praticien&task=showAuth">
-        <img id='praticien' src="assets/images/praticienAccueil.png" alt="accès praticien" />
-    </a>
-    <a href=".?controller=patient&task=showAuth">
-        <img id='patient' src="assets/images/patientAccueil.png" alt="accès patient" />
-    </a>
-</div>
